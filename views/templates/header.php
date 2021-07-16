9ce7533420d0b8e0f88ad8561130c42ac7e4a0f2
Xeuhma! <xhm1@protonmail.com>
2021-07-16 14:02:36 +0200

    retour de paus

--- views/templates/header.php
+++ views/templates/header.php
@@ -4,21 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="./assets/css/styles.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+
+    <title>Accueil</title>
 </head>
-<body>
-    <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="../index.php">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="../afficher_employes.php">Features</a>
-      
-    </div>
-  </div>
-</nav>
+<header href="#">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            
+            <a class="navbar-brand ps-3" href="index.php">BlablaCorp</a>
+            
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-bars"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="index.php">Accueil</a></li>
+                        <li><a class="dropdown-item" href="afficher_employes.php">Employes</a></li>
+                        
+                        
+                        
+                    </ul>
+                </li>
+            </ul>
+        </nav>
     </header>
--- views/update_employes.php
+++ views/update_employes.php
@@ -1,17 +1,13 @@
 <?php
 require_once('../src/controllers/EmployesController.php');
+require_once('../views/templates/header.php');
 
 
-// if(!empty($_POST['id_employes']) || !empty($_POST['prenom']) || !empty($_POST['nom']) || !empty($_POST['sexe']) || !empty($_POST['service']) || !empty($_POST['date_embauche']) || !empty($_POST['salaire'])){
-  
-//     $employesController = new EmployesController($_POST['id_employes'], $_POST['prenom'], $_POST['nom'], $_POST['sexe'], $_POST['service'], $_POST['date_embauche'], $_POST['salaire']);
-//     $employesController->updateEmployes();
-    
-// }
 
-// form pre remplis avec id
 $newEmployes = new EmployesController(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 $employes = $newEmployes->listEmployes();
+
+
 ?>
 
 <section>
@@ -23,41 +19,40 @@ $employes = $newEmployes->listEmployes();
                       <div class="card-body">
                           <form enctype="multipart/form-data" action="update_employes.php" method="post">
                               <div class="form-floating mb-3">
-                                <select name="id_employe" id="id_employe">
+                                    <select name="id_employe" id="id_employe">
                                       <option value="">--Choississez une employe--</option>
                                       <?php foreach($employes as $values): ?>
                                       <option value="<?=$values['id_employes']?>"><?=$values['nom']?></option>
-                                      
-                                </select>
-                                  </div>
-                                  <div class="form-floating mb-3">
-                                      <input class="form-control" id="prenom" type="text" name="prenom" />
+                                      <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <div class="form-floating mb-3">
+                                      <input class="form-control" id="prenom" type="text" name="prenom" placeholder="" />
                                       <label for="prenom">Prénom</label>
-                                  </div>
-                                  <div class="form-floating mb-3">
+                                </div>
+                                <div class="form-floating mb-3">
                                       <input class="form-control" id="nom" type="text" name="nom" />
                                       <label for="nom">Nom</label>
-                                  </div>
-                                  <div class="form-floating mb-3">
+                                </div>
+                                <div class="form-floating mb-3">
                                       <input class="form-control" id="sexe" type="text" name="sexe" />
                                       <label for="sexe">sexe</label>
-                                  </div>
-                                  <div class="form-floating mb-3">
+                                </div>
+                                <div class="form-floating mb-3">
                                       <input type=text id="service" name="service">
                                       <label for="service">Service</label>
-                                  </div>
-                                  <div class="form-floating mb-3">
+                                </div>
+                                <div class="form-floating mb-3">
                                       <input type="text" name="date_embauche" id="date_embauche">
                                       <label for="date_embauche">Date d'embauche</label>
-                                  </div>
-                                  <div class="form-floating mb-3">
+                                </div>
+                                <div class="form-floating mb-3">
                                       <input class="form-control" id="salaire" type="text" name="salaire" />
                                       <label for="salaire">Salaire</label>
-                                  </div>
-                                  <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                </div>
+                                <div class="d-flex align-items-center justify-content-center mt-4 mb-0">
                                       <button class="btn btn-primary" >Mettre à jour</button>
-                                  </div>
-                                  <?php endforeach; ?>
+                                </div>
                           </form>
                       </div>
                     </div>
@@ -66,4 +61,6 @@ $employes = $newEmployes->listEmployes();
       </div>
 </section>
 
-
+<?php
+require_once('../views/templates/footer.php');
+?>
